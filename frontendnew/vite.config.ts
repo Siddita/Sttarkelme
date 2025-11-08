0559b398961ce97a5c222589c8f5f3685d81e37f
Siddita Varma <vsiddita@gmail.com>
2025-11-08 23:26:38 +0530

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- frontendnew/vite.config.ts
+++ frontendnew/vite.config.ts
@@ -6,7 +6,7 @@ import { execSync } from 'child_process';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
